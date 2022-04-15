78fc746f899d5319f280a6646f187fd76daed931
crh <105904533@qq.com>
2022-04-15 22:41:32 +0800

    通知列表 包含任务信息

--- app/Models/WorkNotice.php
+++ app/Models/WorkNotice.php
@@ -27,6 +27,13 @@ class WorkNotice extends Model
     {
         $fillter = [];
         !empty($user_id) && $fillter['user_id'] = $user_id;
-        return self::where($fillter)->select('id as notice_id','name','content','is_read','created_at')->orderByDesc('created_at')->paginate($params['size'] ?? 10);
+        return self::with('taskInfo')->where($fillter)->select('id as notice_id','name','content','is_read','created_at','task_id')->orderByDesc('created_at')->paginate($params['size'] ?? 10);
     }
+
+    public function taskInfo()
+    {
+        return $this->hasOne(Task::class,'id','task_id');
+    }
+
+
 }
