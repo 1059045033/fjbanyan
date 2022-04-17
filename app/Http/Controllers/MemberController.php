294718fb8f9ef9d1a914e1b7911b59161604e47f
crh <105904533@qq.com>
2022-04-17 19:32:22 +0800

    update

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -103,6 +103,7 @@ class MemberController extends Controller
         $list = User::with(['company','Region:id,name'])->when(!empty($params['region_id']), function ($query) use($params){
             $query->where('region_id',$params['region_id']);
         })->where('id','<>',$user['id'])
+            ->whereNotNull('region_id')
             ->where('role',10)->select('id as user_id','name','avator','created_at','phone','image_base64','company_id','region_id')->get();
         $res['belonging'] = $list;
 
