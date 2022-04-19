69feac6b6214a350f8b0c9816fbfd44b579f87d0
crh <105904533@qq.com>
2022-04-19 14:16:09 +0800

    update

--- app/Models/ExceptionMsg.php
+++ app/Models/ExceptionMsg.php
@@ -27,6 +27,6 @@ class ExceptionMsg extends Model
             $data['start'] = strtotime(date('Y-m-d 00:00:00',$tt));
             $data['end']   = strtotime(date('Y-m-d 23:59:59',$tt));
             $query->whereBetween('created_at', [$data['start'], $data['end']]);
-        })->select('id as exception_msg_id','type','content','created_at')->orderByDesc('created_at')->get();
+        })->select('id as exception_msg_id','type','content','created_at')->orderByDesc('created_at')->paginate($params['size'] ?? 10);;
     }
 }
