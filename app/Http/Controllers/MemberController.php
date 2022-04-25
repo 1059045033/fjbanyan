12aca49674190cf82cb4a18bc4316d4bbd2b26b4
crh <105904533@qq.com>
2022-04-25 23:28:39 +0800

    test

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -240,7 +240,7 @@ class MemberController extends Controller
 
     // 添加工作人员到区域
     public function usersToWorkRegion(Request $request)
-    {
+    {echo 999;die;
         $user = $request->user();
 //        $request->validate([
 //            'region_id' => $user['role'] == 30 ? 'required|exists:work_regions,id':'nullable',
@@ -281,13 +281,13 @@ class MemberController extends Controller
             !empty($temp_user->jpush_reg_id) && $jpush_reg_ids[] = $temp_user->jpush_reg_id;
         }
         #=========== 设置工作区域 start =========
-//        JPushService::pushInApp([
-//            'reg_id' =>  $jpush_reg_ids,
-//            'extras' =>  [
-//                'type' => 1,
-//            ],
-//            'type' =>  JPushService::PUSH_TYPE_REG_ID,
-//        ]);
+        JPushService::pushInApp([
+            'reg_id' =>  $jpush_reg_ids,
+            'extras' =>  [
+                'type' => 1,
+            ],
+            'type' =>  JPushService::PUSH_TYPE_REG_ID,
+        ]);
         #=========== 设置工作区域   end =========
 
 
