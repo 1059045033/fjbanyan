ef2aced6b8b094c73831fa6bf9261351258e1ad2
crh <105904533@qq.com>
2022-05-11 14:32:09 +0800

    增加操作记录表相关表 model controller

---
+++ app/Http/Controllers/OptRecordController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OptRecord;
+use App\Http\Requests\StoreOptRecordRequest;
+use App\Http\Requests\UpdateOptRecordRequest;
+
+class OptRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOptRecordRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOptRecordRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OptRecord  $optRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OptRecord $optRecord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OptRecord  $optRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OptRecord $optRecord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOptRecordRequest  $request
+     * @param  \App\Models\OptRecord  $optRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOptRecordRequest $request, OptRecord $optRecord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OptRecord  $optRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OptRecord $optRecord)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_05_11_104427_create_opt_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opt_records', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment("后台人员操作记录表");
+            $table->integer('opt_user_id')->comment("操作人员id");
+            $table->string('opt_user_name')->comment("操作人员账号名")->nullable();
+            $table->string('ip')->comment("登入的ip")->nullable();
+            $table->string('agent')->comment("请求的User-Agent")->nullable();
+            $table->integer('type')->comment("操作类型[1:新增 2:编辑 3:删除]")->nullable();
+            $table->string('desc')->comment("相关描述")->nullable();
+            $table->unsignedInteger('created_at');
+            $table->unsignedInteger('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opt_records');
+    }
+}
