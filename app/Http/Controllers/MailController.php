1c1e669026e6d6e4b02075b840356ece81f49104
crh <105904533@qq.com>
2022-05-13 15:10:09 +0800

    send email

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -8,19 +8,18 @@ use Illuminate\Support\Facades\Mail;
 class MailController extends Controller
 {
     public function send(){
-        //echo '发送邮件';
-        $name = [
-            'name' => "陈荣辉",
-            'message' => date('Y-m-d').' XXX数据打包  点击下载'
+
+        $params = [
+            'to_name' => "尊敬的领导",
+            'message' => date('Y-m-d').'的数据',
+            'data' =>[
+                'task_url' => "http://www.baidu.com",
+                'track_url' => "http://www.baidu.com",
+            ]
         ];
-        // Mail::send()的返回值为空，所以可以其他方法进行判断
-        // Mail::send();需要传三个参数;
-        // 第一个为引用的模板
-        // 第二个为给模板传递的变量（邮箱发送的文本内容）
-        // 第三个为一个闭包，参数绑定Mail类的一个实例。
-        Mail::send('email',['name'=>$name],function($message){
-            $to = '190507753@qq.com';
-            $message ->to($to)->subject('测试邮件标题');
+        Mail::send('email',['params'=>$params],function($message){
+            $to = ['190507753@qq.com','1059045033@qq.com','359448144@qq.com'];
+            $message ->to($to)->subject(date('Y-m-d')."打包数据");
         });
         // 返回的一个错误数组，利用此可以判断是否发送成功
         dd(Mail::failures());
