07b19ca7603963515ed0477ddb331f9a1af85d5f
crh <105904533@qq.com>
2022-05-20 00:31:43 +0800

    update

--- app/Console/Commands/AttendanceLog.php
+++ app/Console/Commands/AttendanceLog.php
@@ -35,7 +35,7 @@ class AttendanceLog extends Command
             # 查找所有的三级和二级人员
             $users = DB::table('users')
                 ->whereIn('role',[10,20])
-                ->where('id',249)
+                //->where('id',249)
                 ->select('id','name','phone','company_id','region_id','work_region_id','role')
                 ->get()
                 ->each(function ($data,$key) use($companies,$regions){
