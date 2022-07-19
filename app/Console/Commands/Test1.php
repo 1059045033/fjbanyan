1513274b5dd0898c492118b680fd31300e7cff81
crh <105904533@qq.com>
2022-07-19 16:33:01 +0800

    update

---
+++ app/Console/Commands/Test1.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\WorkNotice as WorkNoticeModel;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+class Test1 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:bc';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '补充无水印图片';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        # 获取参数 日期参数
+        $day = empty($this->option('day')) ? null:$this->option('day');
+        $day2 = date('Ymd',strtotime($day));
+
+        #获取目录中的所有图片
+        $ppp = 'task_atlas_no_sy';
+        $directories=$this->scanAll(public_path($ppp).DIRECTORY_SEPARATOR.$day2);
+
+        # 日期的起止时间戳
+        //$day   = '2022-07-16';//empty($date_day) ? date('Y-m-d'):$date_day;
+        $start = Carbon::parse($day)->startOfDay()->timestamp;
+        $end   = Carbon::parse($day)->endOfDay()->timestamp;
+
+        foreach ($directories as $k=>$v)
+        {
+            $logs = DB::table('task_log_no_sies_bak')
+                ->where(['user_id'=>$k])
+                ->whereBetWeen('created_at',[$start,$end])
+                ->select('id')
+                ->get()->toArray();
+
+            foreach ($v as $kk=>$vv)
+            {
+                $temp = array_shift($logs);
+                if($temp)
+                {
+                    DB::table('task_log_no_sies_bak')
+                        ->where(['user_id'=>$temp['id']])->update([
+                            'atlas2' =>json_encode($vv)
+                        ]);
+                }
+            }
+        }
+
+        return 0;
+
+    }
+
+    /**
+     * 遍历某个目录下的所有文件
+     * @param string $dir
+     */
+    function scanAll($dir)
+    {
+        $list = array();
+        $list[] = $dir;
+        $res = [];
+
+        while (count($list) > 0)
+        {
+            //弹出数组最后一个元素
+            $file = array_pop($list);
+            //如果是目录
+            if (is_dir($file))
+            {
+                $children = scandir($file);
+                foreach ($children as $child)
+                {
+                    if ($child !== '.' && $child !== '..')
+                    {
+                        $list[] = $file.'/'.$child;
+                    }
+                }
+            }else{
+                //处理当前文件
+                $file = basename($file, ".php");
+                $user_id = explode('_',$file)[0];
+                $res[$user_id][] =  $file;
+            }
+        }
+
+        $newRes = [];
+        foreach ($res as $k=>$v)
+        {
+            foreach ($v as $kk=>$vv)
+            {
+                $temp[] = $vv;
+                if(count($temp) == 3)
+                {
+                    $newRes[$k][] = $temp;
+                    $temp = [];
+                }
+            }
+            if(!empty($temp)){
+                $newRes[$k][] = $temp;
+            }
+        }
+        return $newRes;die;
+    }
+}
